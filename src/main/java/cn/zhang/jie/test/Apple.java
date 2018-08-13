42de3d542f3598463d20fadae25f8ddf06cbceb7
zhangjiee <zhangjieee@1.q>
2018-08-13 23:56:50 +0800

    第一次提交，第一次提交

---
+++ src/main/java/cn/zhang/jie/test/Apple.java
@@ -0,0 +1,8 @@
+package cn.zhang.jie.test;
+
+public class Apple {
+
+	public static void main(String[] args) {
+		System.out.println(1);
+	}
+}
---
+++ src/main/java/cn/zhang/jie/test/Banner.java
@@ -0,0 +1,8 @@
+package cn.zhang.jie.test;
+
+public class Banner {
+
+	public static void main(String[] args) {
+		System.out.println("b");
+	}
+}
